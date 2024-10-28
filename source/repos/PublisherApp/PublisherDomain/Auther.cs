a22e1c4000fbb7d539040e455d1397d49ae1a94f
wafaa alomour <wafaa.alomour@gmail.com>
2024-10-28 11:02:22 +0200

    add domian

---
+++ source/repos/PublisherApp/PublisherDomain/Auther.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PublisherDomain
+{
+    public class Auther
+    {
+        public Auther() { 
+             Books= new List<Book>();
+
+            }
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public List<Book> Books { get; set; }
+    }
+}
---
+++ source/repos/PublisherApp/PublisherDomain/Book.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PublisherDomain
+{
+    public class Book
+    {
+        public int BookId { get; set; }
+        public string Title { get; set; }
+        public DateTime Publishdate { get; set; }
+        public decimal BasePrice {  get; set; }
+        public int AutherId {  get; set; }
+    }
+}
